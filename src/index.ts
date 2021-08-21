a05e6cbd9d0170b3558232bdd26704b558da2124
Jelly-Fish-Dev <isaac.g.hawkins@gmail.com>
2021-08-21 21:36:17 +1000

    expanded previous job bar

--- src/index.ts
+++ src/index.ts
@@ -107,7 +107,7 @@ d3.csv(jobsFile).then((data : d3.DSVRowArray<string>): object => {
 
   let multiselect = d3.select("#jobs")
     .append("select")
-    .attr("multiple", true)
+    .attr("multiple", "true")
   multiselect.selectAll(".pastJobs")
     .data(jobs)
     .enter()
