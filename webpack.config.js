46fa5d84940a590c5cad3aa50e95705786a2b096
Jelly-Fish-Dev <isaac.g.hawkins@gmail.com>
2021-08-22 16:54:57 +1000

    fixed width

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   devServer: {
     compress: true,
